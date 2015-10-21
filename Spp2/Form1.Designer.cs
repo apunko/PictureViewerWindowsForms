cb827316cc1f2635f2084a4d5846f1b646349f89
Anton <ant.punko@gmail.com>
2015-10-21 09:48:33 +0300

    add basic draw ability and panel under pictureBox

--- Spp2/Form1.Designer.cs
+++ Spp2/Form1.Designer.cs
@@ -35,17 +35,29 @@
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.DrawButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(32, 14);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(630, 395);
+            this.pictureBox1.Size = new System.Drawing.Size(208, 247);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
             // openFileDialog1
             // 
@@ -102,20 +114,90 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(667, 194);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Minimum = -100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 6;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(676, 274);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Visible = false;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(667, 245);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(123, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "SizeChangeMode";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(667, 384);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // DrawButton
+            // 
+            this.DrawButton.Location = new System.Drawing.Point(686, 12);
+            this.DrawButton.Name = "DrawButton";
+            this.DrawButton.Size = new System.Drawing.Size(134, 23);
+            this.DrawButton.TabIndex = 11;
+            this.DrawButton.Text = "Enable Draw";
+            this.DrawButton.UseVisualStyleBackColor = true;
+            this.DrawButton.Click += new System.EventHandler(this.DrawButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(2, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(678, 402);
+            this.panel1.TabIndex = 12;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(832, 419);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.DrawButton);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.trackBar2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Picture Viewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -128,6 +210,12 @@
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button DrawButton;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Spp2/Form1.cs
+++ Spp2/Form1.cs
@@ -13,10 +13,17 @@ namespace Spp2
 {
     public partial class Form1 : Form
     {
+        Image LoadedImage;
+        bool EnableDraw = false;
+        bool draw = false;
+        Point priviousPoint;
+        Color color = Color.Red;
         public Form1()
         {
             InitializeComponent();
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            panel1.AutoScroll = true;
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+
         }
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
@@ -29,6 +36,7 @@ namespace Spp2
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 pictureBox1.Load(openFileDialog1.FileName);
+                LoadedImage = pictureBox1.Image;
             }
         }
 
@@ -63,6 +71,138 @@ namespace Spp2
         {
             pictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
             pictureBox1.Refresh();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            float value = trackBar2.Value*0.01f;
+            float[][] colorMatrixElements = { 
+                new float[] {1,  0,  0,  0, 0},        // red scaling factor of 2
+                new float[] {0,  1,  0,  0, 0},        // green scaling factor of 1
+                new float[] {0,  0,  1,  0, 0},        // blue scaling factor of 1
+                new float[] {0,  0,  0,  1, 0},        // alpha scaling factor of 1
+                new float[] {value, value, value, 0, 1}
+            };
+            ColorMatrix colorMatrix = new ColorMatrix(colorMatrixElements);
+            ImageAttributes imageAttributes = new ImageAttributes();
+            imageAttributes.SetColorMatrix(colorMatrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+            Image _img = LoadedImage;
+            Graphics _g = default(Graphics);
+            Bitmap bm_dest = new Bitmap(Convert.ToInt32(_img.Width), Convert.ToInt32(_img.Height));
+            _g = Graphics.FromImage(bm_dest);
+            _g.DrawImage(_img, new Rectangle(0, 0, bm_dest.Width + 1, bm_dest.Height + 1), 0, 0, bm_dest.Width + 1, bm_dest.Height + 1, GraphicsUnit.Pixel, imageAttributes);
+            pictureBox1.Image = bm_dest;
+            pictureBox1.Refresh();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Size modifiedSize = new Size((LoadedImage.Width * trackBar1.Value) / 10, (LoadedImage.Height * trackBar1.Value) / 10);
+            Bitmap bm_source = new Bitmap(LoadedImage);
+            // Make a bitmap for the result.
+            Bitmap bm_dest = new Bitmap(Convert.ToInt32(modifiedSize.Width), Convert.ToInt32(modifiedSize.Height));
+            // Make a Graphics object for the result Bitmap.
+            Graphics gr_dest = Graphics.FromImage(bm_dest);
+            // Copy the source image into the destination bitmap.
+            gr_dest.DrawImage(bm_source, 0, 0, bm_dest.Width + 1, bm_dest.Height + 1);
+            // Display the result.
+            pictureBox1.Image = bm_dest;
+            pictureBox1.Refresh();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.SizeMode == PictureBoxSizeMode.Zoom)
+            {
+                button5.Text = "TurnOffSizeChangedMode";
+                trackBar1.Show();
+                pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            }
+            else
+            {
+                button5.Text = "TurnOnSizeChangedMode";
+                trackBar1.Hide();
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void DrawButton_Click(object sender, EventArgs e)
+        {
+            if (EnableDraw)
+            {
+                DrawButton.Text = "EnableDraw";
+                EnableDraw = false;
+            }
+            else
+            {
+                DrawButton.Text = "DisableDraw";
+                EnableDraw = true;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Up");
+            draw = false;
+        }
+
+        private int TranslateToImageX(int x)
+        {
+            return x;
+        }
+
+        private int TranslateToImageY(int y)
+        {
+            return y;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+           // MessageBox.Show(e.X.ToString() + "/" + e.Y.ToString());
+           // MessageBox.Show(pictureBox1.Image.Height.ToString() + "/" + pictureBox1.Image.Width.ToString());
+            priviousPoint = e.Location;
+            draw = true;
+            /*
+            Graphics graph = Graphics.FromImage(pictureBox1.Image);
+            graph.FillEllipse(new SolidBrush(Color.Red), TranslateToImageY(e.X), TranslateToImageY(e.Y), 5, 5);
+            graph.Save();
+            pictureBox1.Refresh();
+            LoadedImage = (Bitmap)pictureBox1.Image; 
+        */
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {           
+            if (draw && EnableDraw) 
+            {
+                Graphics graph = Graphics.FromImage(pictureBox1.Image);
+                graph.DrawLine(new Pen(new SolidBrush(Color.Red)), priviousPoint, e.Location);
+                priviousPoint = e.Location;
+                //graph.FillEllipse(new SolidBrush(Color.Red), TranslateToImageX(e.X), TranslateToImageY(e.Y), 5, 5); 
+                graph.Save(); 
+                pictureBox1.Refresh(); 
+                LoadedImage = (Bitmap)pictureBox1.Image; 
+
+            } 
+            /*
+            if (draw)
+            {
+                Graphics g = Graphics.FromImage(LoadedImage);
+                SolidBrush brush = new SolidBrush(color);
+                g.FillRectangle(brush, e.X, e.Y, 5, 5);
+                g.Save();
+                pictureBox1.Image = LoadedImage;
+            }*/
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
